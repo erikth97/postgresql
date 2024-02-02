33988f8ed73d45b4bde5c710e7af6b935bbe35b4
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-02-01 22:41:52 -0600

    work_2

---
+++ tareas/03-tareas.sql
@@ -0,0 +1,140 @@
+
+-- 1. Cuantos usuarios tenemos con cuentas @google.com
+-- Tip: count, like
+
+SELECT
+    COUNT (*)
+FROM
+    users
+WHERE
+    email like '%@google.com';
+
+
+-- 2. De qué países son los usuarios con cuentas de @google.com
+-- Tip: distinct
+
+SELECT
+    DISTINCT country
+FROM
+    users
+WHERE
+    email like '%@google.com';
+
+
+
+-- 3. Cuantos usuarios hay por país (country)
+-- Tip: Group by
+
+select
+    count(*),
+    country
+from
+    users
+group BY
+    country;
+
+
+-- 4. Listado de direcciones IP de todos los usuarios de Iceland
+-- Campos requeridos first_name, last_name, country, last_connection
+
+SELECT
+    first_name,
+    last_name,
+    country,
+    last_connection
+FROM
+    users
+WHERE
+    country = 'Iceland';
+
+
+
+-- 5. Cuantos de esos usuarios (query anterior) tiene dirección IP
+-- que incia en 112.XXX.XXX.XXX
+
+select
+    count(*)
+from
+    users
+where
+    country = 'Iceland'
+    and last_connection like '112.%';
+
+
+
+-- 6. Listado de usuarios de Iceland, tienen dirección IP
+-- que inicia en 112 ó 28 ó 188
+-- Tip: Agrupar condiciones entre paréntesis 
+
+select
+    first_name,
+    last_name,
+    country,
+    last_connection
+from
+    users
+where
+    country = 'Iceland'
+    and(
+        last_connection LIKE '112.%'
+        OR last_connection LIKE '28.%'
+        OR last_connection LIKE '188.%'
+    );
+
+-- 7. Ordene el resultado anterior, por apellido (last_name) ascendente
+-- y luego el first_name ascendentemente también
+
+select
+    first_name,
+    last_name,
+    country,
+    last_connection
+from
+    users
+where
+    country = 'Iceland'
+    and(
+        last_connection LIKE '112.%'
+        OR last_connection LIKE '28.%'
+        OR last_connection LIKE '188.%'
+    )
+order by
+	last_name asc, first_name asc;
+    
+
+
+
+-- 8. Listado de personas cuyo país está en este listado
+-- ('Mexico', 'Honduras', 'Costa Rica')
+-- Ordenar los resultados de por País asc, Primer nombre asc, apellido asc
+-- Tip: Investigar IN
+-- Tip2: Ver Operadores de Comparación en la hoja de atajos (primera página)
+
+select
+    country,
+    first_name,
+    last_name
+from
+    users
+where
+    country IN ('Mexico', 'Honduras', 'Costa Rica')
+order by
+    country asc,
+    first_name asc,
+    last_name asc;
+    
+    
+
+-- 9. Del query anterior, cuente cuántas personas hay por país
+-- Ordene los resultados por País asc
+select
+    count(*) as total,
+    country
+from
+    users
+where
+    country IN ('Mexico', 'Honduras', 'Costa Rica')
+group by
+    country
+order by
+    country asc;
